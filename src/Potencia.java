f1a0bd862059c72aeacbca78d23e30f3492c57d8
Felipe S. Pérez <felipeperezruelas@gmail.com>
2022-10-20 22:44:31 -0600

    Potencia modificado, se añade un comentario

--- src/Potencia.java
+++ src/Potencia.java
@@ -1,6 +1,6 @@
 
 public class Potencia {
-
+    //Comentario de prueba
     public long obtenerPotencia(int numeroAElevar, int potencia) {
         if (potencia == 1)
             return numeroAElevar;
